54882cddd3c892263ddeec0bb9ca4f959b65a8b5
Nhan Vo <niick7@gmail.com>
2024-10-19 17:28:25 -0500

    Initialize a Spring MVC with thymeleaf

---
+++ src/main/java/com/example/thymeleaf_demo/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.example.thymeleaf_demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+    @GetMapping("/hello")
+    public String sayHello(Model theModel) {
+        theModel.addAttribute("theDate", java.time.LocalDateTime.now());
+
+        return "hello_world";
+    }
+}
