242fd8111bc445d753d6b213f4e66806faaec82a
Nhan Vo <niick7@gmail.com>
2024-10-21 13:19:02 -0500

    Use Servelet to read the HTML form parameters and adding them to the model

--- src/main/java/com/example/thymeleaf_demo/controller/HelloWorldController.java
+++ src/main/java/com/example/thymeleaf_demo/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.example.thymeleaf_demo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,5 +17,22 @@ public class HelloWorldController {
     @RequestMapping("/processForm")
     public String processForm() {
         return "hello-world";
+    }
+
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+        // Read the request parameter from the HTML form
+        String theName = request.getParameter("studentName");
+
+        // Convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // Create the message
+        String result = "Yo! " + theName;
+
+        // Add message to the model
+        model.addAttribute("message", result);
+
+        return "hello-world-version-two";
     }
 }
