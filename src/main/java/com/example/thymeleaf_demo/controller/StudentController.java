dc1a4998d1950bf14bf72994bf2e56ce91e465f0
Nhan Vo <niick7@gmail.com>
2024-10-21 14:23:23 -0500

    Binding value from application.properties to the StudentController

--- src/main/java/com/example/thymeleaf_demo/controller/StudentController.java
+++ src/main/java/com/example/thymeleaf_demo/controller/StudentController.java
@@ -1,17 +1,28 @@
 package com.example.thymeleaf_demo.controller;
 
 import com.example.thymeleaf_demo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+    // Inject the countries that we defined in the application.properties
+    // To this Student Controller
+    @Value("${countries}")
+    private List<String> countries;
+
     @GetMapping("/student/registration")
     public String registration(Model model) {
         model.addAttribute("student", new Student());
+
+        // Add the list of countries to the model
+        model.addAttribute("countries", countries);
 
         return "students/registration";
     }
